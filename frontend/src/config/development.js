add60389ccf12b31c1708a0832246aad4c232969
Doduydang <doduydang@gmail.com>
2020-07-12 00:09:25 +0700

    2nd times

--- frontend/src/config/development.js
+++ frontend/src/config/development.js
@@ -1,9 +1,17 @@
-const firebaseConfig = <insert firebase config here>;
+const firebaseConfig = {
+  apiKey: 'AIzaSyBe_5YnfYzgwjSJjusRJaPHtMcWK0jJT8g',
+  authDomain: 'pet-hotel-prod-748e8.firebaseapp.com',
+  databaseURL:
+    'https://pet-hotel-prod-748e8.firebaseio.com',
+  projectId: 'pet-hotel-prod-748e8',
+  storageBucket: 'pet-hotel-prod-748e8.appspot.com',
+  messagingSenderId: '18095916881',
+  appId: '1:18095916881:web:0c7d4cd05c32129f7d3570',
+  measurementId: 'G-WDT4RYYQB7',
+};
 
 // Cloud Functions
-const backendUrl = `https://us-central1-${
-  firebaseConfig.projectId
-}.cloudfunctions.net/api/graphql`;
+const backendUrl = `https://us-central1-${firebaseConfig.projectId}.cloudfunctions.net/api/graphql`;
 
 // App Engine
 // const backendUrl = `<insert app engine url here>`;
--- frontend/src/config/localhost.js
+++ frontend/src/config/localhost.js
@@ -1,4 +1,14 @@
-const firebaseConfig = <insert firebase config here>;
+const firebaseConfig = {
+  apiKey: 'AIzaSyBe_5YnfYzgwjSJjusRJaPHtMcWK0jJT8g',
+  authDomain: 'pet-hotel-prod-748e8.firebaseapp.com',
+  databaseURL:
+    'https://pet-hotel-prod-748e8.firebaseio.com',
+  projectId: 'pet-hotel-prod-748e8',
+  storageBucket: 'pet-hotel-prod-748e8.appspot.com',
+  messagingSenderId: '18095916881',
+  appId: '1:18095916881:web:0c7d4cd05c32129f7d3570',
+  measurementId: 'G-WDT4RYYQB7',
+};
 
 // Cloud Functions
 // const backendUrl = `http://localhost:5000/${
